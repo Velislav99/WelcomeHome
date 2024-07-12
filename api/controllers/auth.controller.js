a29ca82590031c03a816ba32507c39e624eef8d1
Velislav99 <velislavchoranov@gmail.com>
2024-07-12 17:56:27 +0200

    .

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10)
 
   const newUser = new User({ username, email, password: hashedPassword });
   try {

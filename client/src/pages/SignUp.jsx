cc3b9a3d986bdc56177a65e2db802119aaab0ddc
Velislav99 <velislavchoranov@gmail.com>
2024-07-15 15:40:29 +0200

    Added signUp page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/signin");
+     
+    }catch (error){
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <Header headerName="SignUp" />
@@ -15,26 +51,31 @@ export default function SignUp() {
             Create your account
           </h1>
         </div>
-        <form className="flex flex-col ">
+        <form onSubmit={handleSubmit} className="flex flex-col ">
           <h2 className="text-secondaryColor text-2xl">Username</h2>
           <input
             type="text"
             className="border border-secondaryColor shadow-sm shadow-gray-500 p-3 rounded-lg mb-4"
             id="username"
+            onChange={handleChange}
           />
           <h2 className="text-secondaryColor text-2xl">Email</h2>
           <input
             type="email"
             className="border border-secondaryColor shadow-sm shadow-gray-500 p-3 rounded-lg mb-4"
             id="email"
+            onChange={handleChange}
           />
           <h2 className="text-secondaryColor text-2xl">Password</h2>
           <input
             type="password"
             className="border border-secondaryColor shadow-sm shadow-gray-500 p-3 rounded-lg mb-4"
             id="password"
+            onChange={handleChange}
           />
-          <button className="bg-mainColor text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 w-48 mx-auto mt-7">Sign Up</button>
+          <button disabled = {loading} className="bg-mainColor text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 w-48 mx-auto mt-7">
+            {loading? "Loading..." : "Sign Up"}
+          </button>
         </form>
         <div className="flex mt-5 gap-2">
           <p className="text-mainColor font-fredoka">Have an account?</p>
@@ -42,6 +83,7 @@ export default function SignUp() {
             <span className="text-mainColor font-fredoka">Sign In</span>
           </Link>
         </div>
+        {error && <p className="text-red-500">{error}</p>}
       </div>
     </>
   );

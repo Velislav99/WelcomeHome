d1e36dbd07eb75337080aad143bc5d2053e82c3c
Velislav99 <velislavchoranov@gmail.com>
2024-07-22 16:51:03 +0200

    Update Header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import About from './pages/About';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Help from "./pages/Help";
+import Profile from "./pages/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/signin' element={<SignIn />} />
-      <Route path='/signup' element={<SignUp />} />
-      <Route path='/about' element={<About />} />
-    </Routes>
-  </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/help" element={<Help />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,11 +3,13 @@ import logo from "../assets/logo.png";
 import { FaSearch, FaRegBell, FaUser } from "react-icons/fa";
 import { AiOutlineMessage } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header({ headerName }) {
+  const { currentUser } = useSelector((state) => state.user);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const dropdownRef = useRef(null);
-  const pages = ["Profile", "Help", "SignIn", "SignUp"];
+  const pages = ["Profile", "Help"];
   useEffect(() => {
     function handleClickOutside(event) {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -42,28 +44,36 @@ export default function Header({ headerName }) {
         <div className="flex items-center">
           <AiOutlineMessage className="text-mainColor mr-4" fontSize={24} />
           <FaRegBell className="text-mainColor mr-4" fontSize={24} />
-          <div className="relative" ref={dropdownRef}>
-            <button
-              onClick={() => setDropdownOpen(!dropdownOpen)}
-              className="focus:outline-none flex items-center"
-            >
-              <FaUser className="text-mainColor" fontSize={24} />
-            </button>
-            {dropdownOpen && (
-              <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
-                {pages.map((page) => (
-                  <Link to={`/${page.toLocaleLowerCase()}`}>
-                    <a
-                      key={page}
-                      className="block px-4 py-2 text-mainColor hover:bg-gray-100"
-                    >
-                      {page}
-                    </a>
-                  </Link>
-                ))}
-              </div>
-            )}
-          </div>
+          {currentUser ? (
+            <div className="relative" ref={dropdownRef}>
+              <button
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+                className="focus:outline-none flex items-center"
+              >
+                <img
+                  src={currentUser.avatar}
+                  alt="Avatar"
+                  className="h-8 w-8 rounded-full "
+                />
+              </button>
+              {dropdownOpen && (
+                <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
+                  {pages.map((page) => (
+                    <Link to={`/${page.toLocaleLowerCase()}`}>
+                      <a
+                        key={page}
+                        className="block px-4 py-2 text-mainColor hover:bg-gray-100"
+                      >
+                        {page}
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+          ) : (
+            <span className="text-mainColor font-fredoka">Sign In</span>
+          )}
         </div>
       </div>
     </header>

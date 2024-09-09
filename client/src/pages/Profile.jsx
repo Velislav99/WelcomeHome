8d4f6b97f8f98b8d986bbe6a68bab57dd500875e
Velislav99 <velislavchoranov@gmail.com>
2024-09-09 14:35:37 +0200

    add delete user

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,7 +8,7 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice.js";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice.js";
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -83,7 +83,24 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   };
+  const handleDeleteUser = async () => {
+    try {
+      dispatch (deleteUserStart())
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      })
 
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch (deleteUserFailure(data.message));
+        return;
+      }
+
+      dispatch (deleteUserSuccess(data));
+    } catch (error) {
+      dispatch (deleteUserFailure(error.message));
+    }
+  }
   return (
     <>
       <Header headerName="Profile" />
@@ -170,7 +187,7 @@ export default function Profile() {
           </div>
         </form>
         <div className="flex justify-between mt-5">
-          <span className="text-red-700 cursor-pointer">Delete Account</span>
+          <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
           <span className="text-red-700 cursor-pointer">Sign Out</span>
         </div>
         <p className="text-red-500 mt-5">{error? error : "" }</p>
